87b713b93611a1debf57e7e9ee24445e891bfb24
Michael A Martinez-Perez <michaelamartinez-perez@Michaels-MacBook-Pro.local>
2019-10-27 21:35:39 -0400

    First commit

---
+++ Challenge2/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody2D rd2d;
+
+    public float speed;
+
+    public Text score;
+
+    public Text winText;
+
+    public int scoreValue = 0;
+
+    public int winScore = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd2d = GetComponent<Rigidbody2D>();
+        score.text = scoreValue.ToString();
+        winText.text = "";
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float hozMovement = Input.GetAxis("Horizontal");
+        float vertMovement = Input.GetAxis("Vertical");
+        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+       
+        if (Input.GetKey("escape"))
+            {
+                Application.Quit();
+            }
+        SetWinText ();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+       if (collision.collider.tag == "Coin")
+        {
+            scoreValue += 1;
+            winScore += 1;
+            score.text = scoreValue.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    void SetWinText ()
+    {
+        if (winScore == 4)
+        {
+            winText.text = "You Win! Game created by Michael Martinez!";
+        }
+    }
+}
